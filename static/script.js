466ef8d14f1796d63ee1eaaf7af6d0b81197ae5f
dimdenGD <firefox78888@gmail.com>
2019-10-27 12:04:36 +0200

    menu, windows

--- static/script.js
+++ static/script.js
@@ -1,22 +1,25 @@
 const { remote } = require('electron');
 const betterSQLite3 = require("better-sqlite3");
-const db = betterSQLite3("../dbs/db.db");
-const res = betterSQLite3("../dbs/res.db");
+const path = require("path");
+const db = betterSQLite3(path.join(__dirname, "../dbs/db.db"));
+const res = betterSQLite3(path.join(__dirname, "../dbs/res.db"));
+const geti = i => document.getElementById(i);
+const config = require("../dbs/options.json");
 
 db.prepare(/*sql*/`create table if not exists proxies (proxy text primary key, type text)`).run();
 db.prepare(/*sql*/`create table if not exists filters (fname text primary key, filter text )`).run();
 
-document.getElementById('minimize').addEventListener('click', () => {
+geti('minimize').addEventListener('click', () => {
     remote.getCurrentWindow().minimize();
 });
 
-document.getElementById('resize').addEventListener('click', () => {
+geti('resize').addEventListener('click', () => {
     const currentWindow = remote.getCurrentWindow();
     if(currentWindow.isMaximized()) currentWindow.unmaximize();
     else currentWindow.maximize();
 });
 
-document.getElementById('exit').addEventListener('click', () => {
+geti('exit').addEventListener('click', () => {
     remote.app.quit();
 });
 
@@ -25,4 +28,17 @@ class PasteCrawler {
         this.crawled = 0;
         this.collected = 0;
     }
+};
+
+const menu = ["home", "proxies", "filters", "results", "options"];
+
+for(let i of menu) {
+    geti(`menu-${i}`).addEventListener("click", () => {
+        for(let j of menu) {
+            geti(`menu-${j}`).className = "menu-btn";
+            geti(`window-${j}`).hidden = true;
+        }
+        geti(`menu-${i}`).className = "menu-btn selected";
+        geti(`window-${i}`).hidden = false;
+    })
 }
